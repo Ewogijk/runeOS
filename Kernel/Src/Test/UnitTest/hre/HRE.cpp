ee098507c8ce2bc7c2ab58f3fa780ce36391e9bb
Matthias Walz <matthiaswalz94@gmail.com>
2025-12-10 19:57:46 +0100

    :sparkles:: Create test report directory if it is missing

--- Kernel/Src/Test/UnitTest/hre/HRE.cpp
+++ Kernel/Src/Test/UnitTest/hre/HRE.cpp
@@ -42,27 +42,51 @@ namespace Heimdall {
     void hre_log_emergency(const HString& message) { hre_log_console(message, VSCODE_RED); }
 
     void hre_save_test_report(const HString& file, const HString& test_report) {
+        hre_log_console(file + ": Save test report.\n");
         Rune::System& sys        = Rune::System::instance();
         auto          vfs_module = sys.get_module<Rune::VFS::VFSModule>(Rune::ModuleSelector::VFS);
         Rune::Path    test_report_file(file.to_c_str());
-        Rune::VFS::IOStatus st =
-            vfs_module->create(test_report_file, Ember::NodeAttribute::FILE | Ember::NodeAttribute::SYSTEM);
+        Rune::Path    test_report_dir = test_report_file.get_parent();
+
+        Rune::VFS::NodeInfo node_info;
+        Rune::VFS::IOStatus st = vfs_module->get_node_info(test_report_dir, node_info);
+        if (st == Rune::VFS::IOStatus::NOT_FOUND) {
+            st = vfs_module->create(test_report_dir,
+                                    Ember::NodeAttribute::DIRECTORY | Ember::NodeAttribute::SYSTEM);
+            if (st != Rune::VFS::IOStatus::CREATED) {
+                hre_log_emergency(HString(test_report_dir.to_string().to_cstr())
+                                  + ": Could not create test report directory. Reason: "
+                                  + st.to_string() + "\n");
+                return;
+            }
+        } else if (st != Rune::VFS::IOStatus::FOUND) {
+            hre_log_emergency(HString(test_report_dir.to_string().to_cstr())
+                              + ": Could not get test report directory node info. Reason: "
+                              + st.to_string() + "\n");
+            return;
+        }
+
+        st = vfs_module->create(test_report_file,
+                                Ember::NodeAttribute::FILE | Ember::NodeAttribute::SYSTEM);
         if (st != Rune::VFS::IOStatus::CREATED) {
-            hre_log_emergency(file + ": Could not create test report file. Reason: " + st.to_string() + "\n");
+            hre_log_emergency(
+                file + ": Could not create test report file. Reason: " + st.to_string() + "\n");
             return;
         }
 
         Rune::SharedPointer<Rune::VFS::Node> node;
         st = vfs_module->open(test_report_file, Ember::IOMode::WRITE, node);
         if (st != Rune::VFS::IOStatus::OPENED) {
-            hre_log_emergency(file  + ": Could not open test report file. Reason: " + st.to_string()+ "\n");
+            hre_log_emergency(file + ": Could not open test report file. Reason: " + st.to_string()
+                              + "\n");
             return;
         }
 
-        Rune::String tr(test_report.to_c_str());
+        Rune::String            tr(test_report.to_c_str());
         Rune::VFS::NodeIOResult io_res = node->write(const_cast<char*>(tr.to_cstr()), tr.size());
         if (io_res.status != Rune::VFS::NodeIOStatus::OKAY) {
-            hre_log_emergency(file  + ": Could not write test report. Reason: " + io_res.status.to_string()+ "\n");
+            hre_log_emergency(file + ": Could not write test report. Reason: "
+                              + io_res.status.to_string() + "\n");
             return;
         }
         node->close();
