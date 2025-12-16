cd479c7224ecc7d64b091f49845986f272924163
Matthias Walz <matthiaswalz94@gmail.com>
2025-12-16 20:00:40 +0100

    :fire:: Remove failed dummy tests

--- Kernel/Include/Test/IntegrationTest/tests/Dummy.h
+++ Kernel/Include/Test/IntegrationTest/tests/Dummy.h
@@ -24,8 +24,8 @@ TEST("Integration Test 1", "A Suite") {
     REQUIRE(1 + 1 == 2)
 }
 
-TEST("Integration Test 2", "A Suite") {
-    REQUIRE(1 + 1 == 3)
-}
+// TEST("Integration Test 2", "A Suite") {
+//     REQUIRE(1 + 1 == 3)
+// }
 
 #endif // RUNEOS_DUMMY_H
