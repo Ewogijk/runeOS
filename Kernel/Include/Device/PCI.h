79e07feceb709bc5dd58850a3cf31b5d2f8b738e
Matthias Walz <matthiaswalz94@gmail.com>
2026-03-20 22:55:53 +0100

    :sparkles: Move PCI implementation from header file to source file

--- Kernel/Include/Device/PCI.h
+++ Kernel/Include/Device/PCI.h
@@ -24,8 +24,6 @@
 #include <Device/AHCI/AHCI.h>
 
 namespace Rune::Device {
-    const SharedPointer<Logger> PCI_LOGGER = LogContext::instance().get_logger("Device.PCI");
-
     union CommandRegister {
         U16 AsUInt16 = 0;
         struct {
@@ -92,202 +90,23 @@ namespace Rune::Device {
         static constexpr U8 REGISTER_OFFSET_MASK  = 0xFC;
 
       public:
-        static auto read_byte(U8 bus, U8 device, U8 func, U8 offset) -> U8 {
-            CPU::out_dw(PCI_CONFIG,
-                        (1 << ENABLE_BIT_SHIFT | (bus << BUS_NUMBER_SHIFT)
-                         | (device << DEVICE_NUMBER_SHIFT) | (func << FUNCTION_NUMBER_SHIFT)
-                         | (offset & REGISTER_OFFSET_MASK)));
-            return CPU::in_b(PCI_DATA + (offset & 0x03));
-        }
+        static auto read_byte(U8 bus, U8 device, U8 func, U8 offset) -> U8;
 
-        static void write_byte(U8 bus, U8 device, U8 func, U8 offset, U8 value) {
-            CPU::out_dw(PCI_CONFIG,
-                        (1 << ENABLE_BIT_SHIFT | (bus << BUS_NUMBER_SHIFT)
-                         | (device << DEVICE_NUMBER_SHIFT) | (func << FUNCTION_NUMBER_SHIFT)
-                         | (offset & REGISTER_OFFSET_MASK)));
-            CPU::out_b(PCI_DATA + (offset & 0x03), value);
-        }
+        static void write_byte(U8 bus, U8 device, U8 func, U8 offset, U8 value);
 
-        static auto read_word(U8 bus, U8 device, U8 func, U8 offset) -> U16 {
-            if ((offset & 0x03) > 2) {
-                return (read_byte(bus, device, func, offset + 1) << SHIFT_8)
-                       | read_byte(bus, device, func, offset);
-            }
-            CPU::out_dw(PCI_CONFIG,
-                        (1 << ENABLE_BIT_SHIFT | (bus << BUS_NUMBER_SHIFT)
-                         | (device << DEVICE_NUMBER_SHIFT) | (func << FUNCTION_NUMBER_SHIFT)
-                         | (offset & REGISTER_OFFSET_MASK)));
-            return CPU::in_w(PCI_DATA + (offset & 0x03));
-        }
+        static auto read_word(U8 bus, U8 device, U8 func, U8 offset) -> U16;
 
-        static void write_word(U8 bus, U8 device, U8 func, U8 offset, U16 value) {
-            if ((offset & 0x03) > 2) {
-                write_byte(bus, device, func, offset, (U8) value);
-                write_byte(bus, device, func, offset + 1, (U8) value >> SHIFT_8);
-            }
-            CPU::out_dw(PCI_CONFIG,
-                        (1 << ENABLE_BIT_SHIFT | (bus << BUS_NUMBER_SHIFT)
-                         | (device << DEVICE_NUMBER_SHIFT) | (func << FUNCTION_NUMBER_SHIFT)
-                         | (offset & REGISTER_OFFSET_MASK)));
-            CPU::out_w(PCI_DATA + (offset & 0x03), value);
-        }
+        static void write_word(U8 bus, U8 device, U8 func, U8 offset, U16 value);
 
-        static auto read_dword(U8 bus, U8 device, U8 func, U8 offset) -> U32 {
-            if ((offset & 0x03) > 0) {
-                return (read_word(bus, device, func, offset + 2) << SHIFT_16)
-                       | read_word(bus, device, func, offset);
-            }
-            CPU::out_dw(PCI_CONFIG,
-                        (1 << ENABLE_BIT_SHIFT | (bus << BUS_NUMBER_SHIFT)
-                         | (device << DEVICE_NUMBER_SHIFT) | (func << FUNCTION_NUMBER_SHIFT)
-                         | (offset & REGISTER_OFFSET_MASK)));
-            return CPU::in_dw(PCI_DATA + (offset & 0x03));
-        }
+        static auto read_dword(U8 bus, U8 device, U8 func, U8 offset) -> U32;
 
-        static void write_dword(U8 bus, U8 device, U8 func, U8 offset, U32 value) {
-            if ((offset & 0x03) > 2) {
-                write_word(bus, device, func, offset, (uint16_t) value);
-                write_word(bus, device, func, offset + 2, (uint16_t) value >> SHIFT_16);
-            }
-            CPU::out_dw(PCI_CONFIG,
-                        (1 << ENABLE_BIT_SHIFT | (bus << BUS_NUMBER_SHIFT)
-                         | (device << DEVICE_NUMBER_SHIFT) | (func << FUNCTION_NUMBER_SHIFT)
-                         | (offset & REGISTER_OFFSET_MASK)));
-            CPU::out_dw(PCI_DATA + (offset & 0x03), value);
-        }
+        static void write_dword(U8 bus, U8 device, U8 func, U8 offset, U32 value);
 
-        static auto read_header(U8 bus, U8 device, U8 func) -> PCIHeader {
-            constexpr U8 VENDOR_ID_OFFSET             = 0x00;
-            constexpr U8 DEVICE_ID_OFFSET             = 0x02;
-            constexpr U8 COMMAND_OFFSET               = 0x04;
-            constexpr U8 STATUS_OFFSET                = 0x06;
-            constexpr U8 REVISION_ID_OFFSET           = 0x08;
-            constexpr U8 PROGRAMMING_INTERFACE_OFFSET = 0x09;
-            constexpr U8 SUB_CLASS_CODE_OFFSET        = 0x0A;
-            constexpr U8 BASE_CLASS_CODE_OFFSET       = 0x0B;
-            constexpr U8 CACHE_LINE_SIZE_OFFSET       = 0x0C;
-            constexpr U8 LATENCY_TIMER_OFFSET         = 0x0D;
-            constexpr U8 HEADER_TYPE_OFFSET           = 0x0E;
-            constexpr U8 BIST_OFFSET                  = 0x0F;
-            return {
-                .vendor_id             = read_word(bus, device, func, VENDOR_ID_OFFSET),
-                .device_id             = read_word(bus, device, func, DEVICE_ID_OFFSET),
-                .command               = {.AsUInt16 = read_word(bus, device, func, COMMAND_OFFSET)},
-                .status                = read_word(bus, device, func, STATUS_OFFSET),
-                .revision_id           = read_byte(bus, device, func, REVISION_ID_OFFSET),
-                .programming_interface = read_byte(bus, device, func, PROGRAMMING_INTERFACE_OFFSET),
-                .sub_class_code        = read_byte(bus, device, func, SUB_CLASS_CODE_OFFSET),
-                .base_class_code       = read_byte(bus, device, func, BASE_CLASS_CODE_OFFSET),
-                .cache_line_size       = read_byte(bus, device, func, CACHE_LINE_SIZE_OFFSET),
-                .latency_timer         = read_byte(bus, device, func, LATENCY_TIMER_OFFSET),
-                .header_type           = read_byte(bus, device, func, HEADER_TYPE_OFFSET),
-                .bist                  = read_byte(bus, device, func, BIST_OFFSET),
-            };
-        }
+        static auto read_header(U8 bus, U8 device, U8 func) -> PCIHeader;
 
-        static void check_device(AHCIDriver& ahci_driver, U8 bus, U8 device) {
-            constexpr U16 INVALID_VENDOR = 0xFFFF;
-            PCIHeader     header         = read_header(bus, device, 0);
-            if (header.vendor_id == INVALID_VENDOR) return;
+        static void check_device(AHCIDriver& ahci_driver, U8 bus, U8 device);
 
-            PCI_LOGGER->debug("Bus: {}, Device: {}, Function: {} - {:#x}:{:#x} - Base Class Code: "
-                              "{:#x} - Sub Class: {:#x} "
-                              "- Programming Interface: {:#x}",
-                              bus,
-                              device,
-                              0,
-                              header.vendor_id,
-                              header.device_id,
-                              header.base_class_code,
-                              header.sub_class_code,
-                              header.programming_interface);
-
-            constexpr U8  AHCI_QEMU_BASE_CLASS     = 0x1;
-            constexpr U8  AHCI_QEMU_SUB_CLASS_CODE = 0x6;
-            constexpr U16 AHCI_QEMU_VENDOR_ID      = 0x8086;
-            constexpr U16 AHCI_QEMU_DEVICE_ID      = 0x2922;
-            if (header.base_class_code == AHCI_QEMU_BASE_CLASS
-                && header.sub_class_code == AHCI_QEMU_SUB_CLASS_CODE
-                && header.vendor_id == AHCI_QEMU_VENDOR_ID
-                && header.device_id == AHCI_QEMU_DEVICE_ID) {
-                write_word(bus, device, 0, 0x04, header.command.AsUInt16 | 0x4); // Enable DMA
-                constexpr U8   BAR_0_OFFSET                      = 0x10;
-                constexpr U8   BAR_1_OFFSET                      = 0x14;
-                constexpr U8   BAR_2_OFFSET                      = 0x18;
-                constexpr U8   BAR_3_OFFSET                      = 0x1C;
-                constexpr U8   BAR_4_OFFSET                      = 0x20;
-                constexpr U8   BAR_5_OFFSET                      = 0x24;
-                constexpr U8   CARD_BUS_CIS_POINTER_OFFSET       = 0x28;
-                constexpr U8   SUBSYSTEM_VENDOR_ID_OFFSET        = 0x2C;
-                constexpr U8   SUBSYSTEM_ID_OFFSET               = 0x2E;
-                constexpr U8   EXPANSION_ROM_ADDRESS_BASE_OFFSET = 0x30;
-                constexpr U8   CAPABILITIES_POINTER_OFFSET       = 0x34;
-                constexpr U8   INTERRUPT_LINE_OFFSET             = 0x3C;
-                constexpr U8   INTERRUPT_PIN_OFFSET              = 0x3D;
-                constexpr U8   MAX_GRANT_OFFSET                  = 0x3E;
-                constexpr U8   MAX_LATENCY_OFFSET                = 0x3F;
-                PCIHeaderType0 ahci_header                       = {
-                                          .header               = read_header(bus, device, 0),
-                                          .bar_0                = read_dword(bus, device, 0, BAR_0_OFFSET),
-                                          .bar_1                = read_dword(bus, device, 0, BAR_1_OFFSET),
-                                          .bar_2                = read_dword(bus, device, 0, BAR_2_OFFSET),
-                                          .bar_3                = read_dword(bus, device, 0, BAR_3_OFFSET),
-                                          .bar_4                = read_dword(bus, device, 0, BAR_4_OFFSET),
-                                          .bar_5                = read_dword(bus, device, 0, BAR_5_OFFSET),
-                                          .card_bus_cis_pointer = read_dword(bus, device, 0, CARD_BUS_CIS_POINTER_OFFSET),
-                                          .subsystem_vendor_id  = read_word(bus, device, 0, SUBSYSTEM_VENDOR_ID_OFFSET),
-                                          .subsystem_id         = read_word(bus, device, 0, SUBSYSTEM_ID_OFFSET),
-                                          .expansion_rom_address_base =
-                        read_dword(bus, device, 0, EXPANSION_ROM_ADDRESS_BASE_OFFSET),
-                                          .capabilities_pointer = read_byte(bus, device, 0, CAPABILITIES_POINTER_OFFSET),
-                                          .reserved_0           = 0,
-                                          .reserved_1           = 0,
-                                          .reserved_2           = 0,
-                                          .interrupt_line       = read_byte(bus, device, 0, INTERRUPT_LINE_OFFSET),
-                                          .interrupt_pin        = read_byte(bus, device, 0, INTERRUPT_PIN_OFFSET),
-                                          .min_grant            = read_byte(bus, device, 0, MAX_GRANT_OFFSET),
-                                          .max_latency          = read_byte(bus, device, 0, MAX_LATENCY_OFFSET),
-                };
-                volatile auto* hba = reinterpret_cast<HBAMemory*>(
-                    Memory::physical_to_virtual_address(ahci_header.bar_5));
-                if (!ahci_driver.start(hba)) {
-                    PCI_LOGGER->error("Failed to init AHCI");
-                    while (true);
-                }
-            }
-
-            constexpr U8 FUNC_LIMIT            = 8;
-            constexpr U8 MULTI_FUNCTION_DEVICE = 0x80;
-            if ((header.header_type & MULTI_FUNCTION_DEVICE) != 0) {
-                for (U8 func = 1; func < FUNC_LIMIT; func++) {
-                    header = read_header(bus, device, 0);
-                    if (header.vendor_id == INVALID_VENDOR) continue;
-
-                    PCI_LOGGER->debug(
-                        "Bus: {}, Device: {}, Function: {} - {:#x}:{:#x} - Base Class "
-                        "Code: {:#x} - Sub "
-                        "Class: {:#x} - Programming Interface: {:#x}",
-                        bus,
-                        device,
-                        func,
-                        header.vendor_id,
-                        header.device_id,
-                        header.base_class_code,
-                        header.sub_class_code,
-                        header.programming_interface);
-                }
-            }
-        }
-
-        static void discover_devices(AHCIDriver& ahci_driver) {
-            constexpr U8 BUS_LIMIT    = 255;
-            constexpr U8 DEVICE_LIMIT = 32;
-            for (U8 bus = 0; bus < BUS_LIMIT; bus++) {
-                for (U8 device = 0; device < DEVICE_LIMIT; device++) {
-                    check_device(ahci_driver, bus, device);
-                }
-            }
-        }
+        static void discover_devices(AHCIDriver& ahci_driver);
     };
 } // namespace Rune::Device
 
