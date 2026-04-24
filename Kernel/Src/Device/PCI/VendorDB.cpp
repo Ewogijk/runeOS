ace692c62a5b945686389914c4b6e6ef3fca3066
Matthias Walz <matthiaswalz94@gmail.com>
2026-04-24 23:44:48 +0200

    :sparkles: Add a Vendor DB and class code enums to resolve ID's to names

---
+++ Kernel/Src/Device/PCI/VendorDB.cpp
@@ -0,0 +1,57 @@
+/*
+ *  Copyright 2025 Ewogijk
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#include <Device/PCI/VendorDB.h>
+
+#include <KRE/Collections/HashMap.h>
+
+namespace Rune::Device {
+
+    auto operator==(const PCIVendorDBRequest& lhs, const PCIVendorDBRequest& rhs) -> bool {
+        return lhs.m_vendor_ID == rhs.m_vendor_ID && lhs.m_device_ID == rhs.m_device_ID;
+    }
+    auto operator!=(const PCIVendorDBRequest& lhs, const PCIVendorDBRequest& rhs) -> bool {
+        return !(lhs == rhs);
+    }
+
+    HashMap<PCIVendorDBRequest, PCIVendorDBResponse> VENDOR_DATABASE;
+
+    void pci_vendor_db_initialize() {
+        VENDOR_DATABASE.put({.m_vendor_ID = 0x8086, .m_device_ID = 0x1237},
+                            {.m_vendor_name = "Intel Corporation",
+                             .m_device_name = "440FX - 82441FX PMC [Natoma]"});
+        VENDOR_DATABASE.put({.m_vendor_ID = 0x8086, .m_device_ID = 0x7000},
+                            {.m_vendor_name = "Intel Corporation",
+                             .m_device_name = "82371SB PIIX3 ISA [Natoma/Triton II]"});
+        VENDOR_DATABASE.put({.m_vendor_ID = 0x8086, .m_device_ID = 0x7010},
+                            {.m_vendor_name = "Intel Corporation",
+                             .m_device_name = "82371SB PIIX3 IDE [Natoma/Triton II]"});
+        VENDOR_DATABASE.put(
+            {.m_vendor_ID = 0x8086, .m_device_ID = 0x7113},
+            {.m_vendor_name = "Intel Corporation", .m_device_name = "82371AB/EB/MB PIIX4 ACPI"});
+        VENDOR_DATABASE.put(
+            {.m_vendor_ID = 0x8086, .m_device_ID = 0x2922},
+            {.m_vendor_name = "Intel Corporation",
+             .m_device_name = "82801IR/IO/IH (ICH9R/DO/DH) 6 port SATA Controller [AHCI mode]"});
+    }
+
+    auto pci_vendor_db_resolve(PCIVendorDBRequest vendor_db_request) -> PCIVendorDBResponse {
+        auto maybe_vendor_db_response = VENDOR_DATABASE.find(vendor_db_request);
+        if (maybe_vendor_db_response == VENDOR_DATABASE.end())
+            return {.m_vendor_name = "Unknown", .m_device_name = "Unknown"};
+        return *maybe_vendor_db_response->value;
+    }
+} // namespace Rune::Device
